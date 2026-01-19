2c9adfee64428fb755654700a5bdb2eb41d9a6ae
hhsk908 <hhsk0908@naver.com>
2026-01-19 10:38:50 +0900

    feat : add DndPolicy and RetryPolicy

--- message/src/main/java/org/backend/message/policy/DndPolicy.java
+++ message/src/main/java/org/backend/message/policy/DndPolicy.java
@@ -1,5 +1,53 @@
 package org.backend.message.policy;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import org.backend.core.message.entity.Message;
+import org.backend.core.user.entity.BanTime;
+import org.backend.core.user.repository.BanTimeRepository;
+import org.backend.message.common.dto.MessageSendEvent;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class DndPolicy {
+	
+	private final BanTimeRepository banTimeRepository;
+	
+	public boolean isDndNow(Message message) {
+		
+		BanTime dnd = banTimeRepository.findBanTimeByMessageId(message.getId()).orElseThrow(
+				() -> new IllegalArgumentException("BenTime not found : " + message.getId()));
+		
+        LocalTime now = LocalTime.now();
+        
+        return now.isAfter(dnd.getStartTime()) || now.isBefore(dnd.getEndTime());
+        
+    }
+
+    public LocalDateTime nextAvailableTime(Message message) {
+    	
+    	BanTime dnd = banTimeRepository.findBanTimeByMessageId(message.getId())
+                .orElseThrow(() -> new IllegalArgumentException("BenTime not found : " + message.getId()));
+    	
+    	LocalDateTime now = LocalDateTime.now();
+        LocalTime startTime = dnd.getStartTime();
+        LocalTime endTime = dnd.getEndTime();
+    	
+    	if (!isDndNow(message)) {
+            return now;
+        }
+    	
+    	LocalDateTime nextTime = now.with(endTime);
+    	
+    	if (nextTime.isBefore(now)) {
+            nextTime = nextTime.plusDays(1);
+        }
+
+        return nextTime;
+    }
 
 }
