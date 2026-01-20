c236301a4591abf57957b09c5c2c8853697fdf95
bakyunsu00 <zgustn0@gmail.com>
2026-01-20 15:03:56 +0900

    feat: 정산 배치 기능 추가 - 배치 결과 billingHistory에 적재 - 중복 방지는 스케줄러 도입하면서 추가 예정 - 기존 BillingHistory 테이블 usage 컬럼 INT -> DECIMAL(20,2)로 수정 필요

---
+++ billing-batch/src/main/java/org/backend/billingbatch/dto/ContractInfo.java
@@ -0,0 +1,26 @@
+package org.backend.billingbatch.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Data
+public class ContractInfo {
+    private Long line_id;
+    private Long user_id;
+    private Long plan_id;
+    private LocalDateTime startDate;
+
+    private BigDecimal base_price;
+
+    private BigDecimal voiceLimit;
+    private BigDecimal voiceUnitPrice;
+
+    private BigDecimal dataLimit;
+    private BigDecimal dataUnitPrice;
+
+    private BigDecimal discountRate;
+    private BigDecimal discountLimit;
+
+
+}
