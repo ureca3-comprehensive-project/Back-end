106103a71d9b43d702b808ec873750eb19a9df38
hhsk908 <hhsk0908@naver.com>
2026-01-21 00:02:23 +0900

    fix : update findByBanTimeByMessageId JPQL

--- core/src/main/java/org/backend/core/user/repository/BanTimeRepository.java
+++ core/src/main/java/org/backend/core/user/repository/BanTimeRepository.java
@@ -10,11 +10,11 @@ import org.springframework.data.repository.query.Param;
 public interface BanTimeRepository extends JpaRepository<BanTime, Integer>{
 	
 	@Query("SELECT b FROM BanTime b " +
-	           "JOIN b.user u " +
-	           "JOIN Line l ON l.user.userId = u.userId " +
-	           "JOIN Invoice i ON i.line.lineId = l.lineId " +
-	           "JOIN Message m ON m.invoice.invoiceId = i.invoiceId " +
-	           "WHERE m.messageId = :messageId")
+			"JOIN b.user u " +
+			"JOIN Line l ON l.user = u " +         // Line 엔티티의 user 필드와 조인
+			"JOIN Invoice i ON i.line = l " +       // Invoice 엔티티의 line 필드와 조인
+			"JOIN Message m ON m.invoice = i " +    // Message 엔티티의 invoice 필드와 조인
+			"WHERE m.id = :messageId")
 	    Optional<BanTime> findBanTimeByMessageId(@Param("messageId") Long messageId);
 	
 	
