d98e43d51ad4b8a50d72d864be69dffad04d88d4
hhsk908 <hhsk0908@naver.com>
2026-01-21 00:28:37 +0900

    fix : add @EnableKafka annotation MessageApplication

--- message/src/main/java/org/backend/message/MessageApplication.java
+++ message/src/main/java/org/backend/message/MessageApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication(scanBasePackages = {
 	    "org.backend.message",
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 	})
 @EntityScan(basePackages = "org.backend.core")
 @EnableJpaRepositories(basePackages = "org.backend.core")
+@EnableKafka
 public class MessageApplication {
     public static void main(String[] args) {
         SpringApplication.run(MessageApplication.class, args);
