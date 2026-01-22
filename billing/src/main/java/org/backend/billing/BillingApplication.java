f3ea487f50991a227ab3754e5111bc636c06b567
bakyunsu00 <zgustn0@gmail.com>
2026-01-23 03:10:03 +0900

    feat: 수동 정산 기능 추가 - billing 모듈에 controller 추가 - billing 모듈 런타임 시에 billing-batch 모듈 의존 - postman으로 수동 배치 실시 및 db적재 확인 완료

--- billing/src/main/java/org/backend/billing/BillingApplication.java
+++ billing/src/main/java/org/backend/billing/BillingApplication.java
@@ -4,8 +4,10 @@ import org.backend.billing.message.service.InMemoryStores;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.backend")
 public class BillingApplication {
 
     public static void main(String[] args) {
---
+++ core/src/main/java/org/backend/port/BatchCommand.java
@@ -0,0 +1,4 @@
+package org.backend.port;
+
+public record BatchCommand(String jobName, String date) {
+}
