0ab7f54495791f4368db13d5fe3ff58ad820d29d
hhsk908 <hhsk0908@naver.com>
2026-01-23 21:11:29 +0900

    fkx : merge conflict

--- billing/src/main/java/org/backend/billing/BillingApplication.java
+++ billing/src/main/java/org/backend/billing/BillingApplication.java
@@ -1,22 +1,17 @@
 package org.backend.billing;
 
-import org.backend.billing.message.service.InMemoryStores;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "org.backend")
+@SpringBootApplication(scanBasePackages = "org.backend")
+@EntityScan(basePackages = "org.backend.domain")
+@EnableJpaRepositories(basePackages = "org.backend.domain")
 public class BillingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BillingApplication.class, args);
-    }
-
-    @Bean
-    public InMemoryStores inMemoryStores() {
-        return new InMemoryStores();
     }
 }
 
